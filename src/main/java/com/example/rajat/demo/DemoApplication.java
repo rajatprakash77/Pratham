59d32f120928d0788af52ebc527e2abeb847f779
rajatprakash77 <prakash.rajat@gmail.com>
2019-12-09 14:58:21 -0500

    spring postgres initialization app changes

--- src/main/java/com/example/rajat/demo/DemoApplication.java
+++ src/main/java/com/example/rajat/demo/DemoApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Add imports
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +17,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String hello() {
+	private JdbcTemplate jdbcTemplate;
+	//@GetMapping("/")
+	/*public String hello() {
 		return "hello rajat!";
+	}*/
+    public DemoApplication(JdbcTemplate jdbcTemplate){this.jdbcTemplate =jdbcTemplate;}
+	@GetMapping("/getTuples")
+	public List<String> getTuples() {
+		return this.jdbcTemplate.queryForList("SELECT * FROM users").stream()
+				.map((m) -> m.values().toString())
+				.collect(Collectors.toList());
 	}
 }
